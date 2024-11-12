5ffb0e88be80556c4eb5fbc2375d05812affeed6
julia <julie.tataurova@yandex.ru>
2024-11-13 01:35:52 +0300

    changed pcache

--- src/pcache/pmain.cpp
+++ src/pcache/pmain.cpp
@@ -1,25 +1,24 @@
 #include "pcache.hpp"
-#include <fstream>
 #include <iostream>
-#include <limits>
+#include <istream>
 
 int get_page(int key) {return key;};
 
-int manage_input_perfect_cache(std::istream &is)
+int manage_input_perfect_cache()
 {
     int cache_size = 0;
     int Npages = 0;
 
-    is >> cache_size;
+    std::cin >> cache_size;
 
-    is >> Npages;
+    std::cin >> Npages;
     std::unordered_map<int, std::vector<int>> map;
     std::vector<int> input;
 
     int key = 0;
-    for(int i_page = 0; i_page < Npages; i_page++)
+    for(int i_page = 0; i_page < Npages; ++i_page)
     {
-        is >> key;
+        std::cin >> key;
         auto found = map.find(key);
         if(found == map.end())
         {
@@ -31,25 +30,17 @@ int manage_input_perfect_cache(std::istream &is)
         }
         input.push_back(key);
     }
-    is >> key;
-    is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
     pcache_t<int, int> pcache(cache_size, map, input);
     return pcache.count_hits(get_page);
 }
 
 int main()
 {
-    std::ifstream file_r {"./tests/data_30_2000.txt"};
-    std::ofstream file_w {"pcache_res.txt"};
     int p_hits = 0;
-    for (int i = 0; i < 100; i++)
-    {
-        p_hits = manage_input_perfect_cache(file_r);
-        file_w << p_hits << std::endl;
-    }
-    file_r.close();
-    file_w.close();
+
+    p_hits = manage_input_perfect_cache();
+    std::cout << p_hits << std::endl;
+
     return 0;
 }
 
